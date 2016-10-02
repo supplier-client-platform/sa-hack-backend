d76a4cf5b96858e5f0e525168b8a88448d833268
Vishan Jayasinghearachchi <vishandanura@hotmail.com>
2016-10-02 13:50:55 +0530

    CORS issue fixed.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,8 +12,9 @@
 */
 
 // HACK: Workaround for CORS issue
-header('Access-Control-Allow-Origin: *');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 Route::get('/', function () {
     return view('welcome');
