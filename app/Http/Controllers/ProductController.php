41b627fa78b312cf3851f46a5399cd48452f356d
Vishan Jayasinghearachchi <vishandanura@hotmail.com>
2016-10-02 12:52:07 +0530

    Product Controller method implementation. index, index_get, store completed.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
+use Exception;
 
 use App\Http\Requests;
 
@@ -15,7 +17,31 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            return [
+                'products' => Product::all()
+            ];
+        }
+        catch (Exception $e) {
+            return response('Error retrieving products', 500);
+        }
+    }
+
+    /**
+     * Display a listing of the resource by id.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index_id($id)
+    {
+        try {
+            return [
+                'product' => Product::findOrFail($id)
+            ];
+        }
+        catch (Exception $e) {
+            return response('Error retrieving product', 404);
+        }
     }
 
     /**
@@ -26,7 +52,35 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+            if (!$request->has('name')) {
+                throw new Exception('Error: name not defined');
+            }
+            if (!$request->has('price')) {
+                throw new Exception('Error: price not defined');
+            }
+            if (!$request->has('img_url')) {
+                throw new Exception('Error: img_url not defined');
+            }
+        }
+        catch (Exception $e) {
+            return response('Incomplete data set', 400);
+        }
+
+        try {
+            $new_product = Product::create([
+                'name' => $request->input('name'),
+                'price' => $request->input('price'),
+                'img_url' => $request->input('img_url')
+            ]);
+
+            return [
+                'product' => $new_product
+            ];
+        }
+        catch(Exception $e) {
+            return response('Error creating product', 500);
+        }
     }
 
     /**
